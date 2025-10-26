dfa88a4f777941a24867fa4271a7ea752be43895
amir-9 <aghaamirjan13@gmail.com>
2025-10-26 22:01:35 +0330

    updated color

--- components/utils/Footer.tsx
+++ components/utils/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const t = useTranslations("landing");
 
   return (
-    <footer className="relative text-center p-4 text-white z-10">
+    <footer className="relative text-center p-4 text-black z-10">
       {t("footer")}
     </footer>
   );
